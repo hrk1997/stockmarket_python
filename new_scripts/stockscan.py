5675ae7ad4682aef690c404cd3ec1b10681b7796
Hrithik Krishna <hrk999666333@gmail.com>
2021-07-30 12:41:37 +0530

    stockacan.py to get bref stock details

--- new_scripts/stockscan.py
+++ new_scripts/stockscan.py
@@ -1,68 +1,66 @@
-#!/bin/python3
 from selenium import webdriver
 browser = webdriver.Chrome()
-browser.get('https://www.moneycontrol.com/indian-indices/nifty-50-9.html')
+browser.get('https://www.moneycontrol.com/india/stockpricequote/banks-public-sector/statebankindia/SBI')
 
-#collectvalue using css selector
-focus = browser.find_element_by_css_selector('#mc_mainWrapper > div > div.sec_indice_detail > div.indices_namntab.clearfix > h1 > div')
-day_preclose = browser.find_element_by_css_selector('#sp_previousclose')
-day_open = browser.find_element_by_css_selector('#sp_open')
-day_high = browser.find_element_by_css_selector('#sp_High')
-day_low = browser.find_element_by_css_selector('#sp_low')
-day_yrhigh = browser.find_element_by_css_selector('#sp_yrhigh')
-day_yrlow = browser.find_element_by_css_selector('#sp_yrlow')
+open = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(1) > table > tbody > tr:nth-child(1) > td.nseopn.bseopn')
+preclose = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(1) > table > tbody > tr:nth-child(2) > td.nseprvclose.bseprvclose')
+volume = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(1) > table > tbody > tr:nth-child(3) > td.nsevol.bsevol')
 
-#store the value to variables
-name       = focus.text 
-preclose   = day_preclose.text
-day_open   = day_open.text
-day_high   = day_high.text
-day_low    = day_low.text
-day_yrhigh = day_yrhigh.text
-day_yrlow  = day_yrlow.text
+value = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(1) > table > tbody > tr:nth-child(4) > td.nsevalue.bsevalue')
+vwap = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(1) > table > tbody > tr:nth-child(5) > td.nsevwap.bsevwap')
 
-#print the stored values
-print("        ",name) 
-print("previous close : ",preclose)
-print("open price     : ",day_open)
-print("day high       : ",day_high)
-print("day low        : ",day_low)
-print("year high      : ",day_yrhigh)
-print("year low       : ",day_yrlow)
-print("                            ")
+m_high    = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(2) > table > tbody > tr:nth-child(1) > td.nseHP.bseHP')
+m_low     = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(2) > table > tbody > tr:nth-child(2) > td.nseLP.bseLP')
+m_uclimit = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(2) > table > tbody > tr:nth-child(3) > td.nseupper_circuit_limit.bseupper_circuit_limit')
+m_lclimit = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(2) > table > tbody > tr:nth-child(4) > td.nselower_circuit_limit.bselower_circuit_limit')
+m_yrhigh  = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(2) > table > tbody > tr:nth-child(5) > td.nseH52.bseH52')
+m_yrlow   = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(2) > table > tbody > tr:nth-child(6) > td.nseL52.bseL52')
+m_ttm_eps = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(3) > table > tbody > tr:nth-child(1) > td.nseceps.bseceps')
+m_ttm_pe  = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(3) > table > tbody > tr:nth-child(2) > td.nsepe.bsepe')
+m_sector_pe  = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(3) > table > tbody > tr:nth-child(3) > td.nsesc_ttm.bsesc_ttm')
+m_book_value = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(3) > table > tbody > tr:nth-child(4) > td.nsebv.bsebv')
+m_p_b        = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(3) > table > tbody > tr:nth-child(5) > td.nsepb.bsepb')
+m_face_value = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(3) > table > tbody > tr:nth-child(6) > td.nsefv.bsefv')
+m_market_cap = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(4) > table > tbody > tr:nth-child(1) > td.nsemktcap.bsemktcap')
+m_divident   = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(4) > table > tbody > tr:nth-child(2) > td.nsedy.bsedy')
+m_avgvoleume  = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(4) > table > tbody > tr:nth-child(3) > td.nsev20a.bsev20a')
+m_avgdelivery = browser.find_element_by_css_selector('#stk_overview > div.nsestock_overview.bsestock_overview > div.mob-hide > div:nth-child(4) > table > tbody > tr:nth-child(4) > td.nsed20ad.bsed20ad')
+m_stock = browser.find_element_by_css_selector('#stockName > h1')
 
-browser.get('https://www.moneycontrol.com/indian-indices/nifty-bank-23.html')
+#= browser.find_element_by_css_selector('')
+stock        =m_stock.text
+high         = m_high.text
+low          = m_low.text
+uclimit      = m_uclimit.text
+lclimit      = m_lclimit.text
+yrhigh       = m_yrhigh.text
+yrlow        = m_yrlow.text
+ttm_eps      = m_ttm_eps.text
+ttm_pe       = m_ttm_pe.text
+sector_pe    = m_sector_pe.text
+bool_value   = m_book_value.text
+pb           = m_p_b.text
+face_value   = m_face_value.text
+market_cap   = m_market_cap.text
+divident     = m_divident.text
+avg_voleme   = m_avgvoleume.text
+avg_delivery = m_avgdelivery.text
 
-#collectvalue using css selector
-focus = browser.find_element_by_css_selector('#mc_mainWrapper > div > div.sec_indice_detail > div.indices_namntab.clearfix > h1 > div')
-day_preclose = browser.find_element_by_css_selector('#sp_previousclose')
-day_open = browser.find_element_by_css_selector('#sp_open')
-day_high = browser.find_element_by_css_selector('#sp_High')
-day_low = browser.find_element_by_css_selector('#sp_low')
-day_yrhigh = browser.find_element_by_css_selector('#sp_yrhigh')
-day_yrlow = browser.find_element_by_css_selector('#sp_yrlow')
-
-
-#table_index =browser.find_element_by_css_selector('#indi_contribute > div.clearfix > div.contribut_bx.FL > table')
-
-#store the value to variables
-name       = focus.text 
-preclose   = day_preclose.text
-day_open   = day_open.text
-day_high   = day_high.text
-day_low    = day_low.text
-day_yrhigh = day_yrhigh.text
-day_yrlow  = day_yrlow.text
-
-
-#print the stored values
-print("        ",name) 
-print("previous close : ",preclose)
-print("open price     : ",day_open)
-print("day high       : ",day_high)
-print("day low        : ",day_low)
-print("year high      : ",day_yrhigh)
-print("year low       : ",day_yrlow)
-print("                            ")
-
+print("     ",stock,"     ")
+print("high         : ",high)
+print("low          : ",low)
+print("uclimit      : ",uclimit)
+print("lclimit      : ",lclimit)
+print("yrhigh       : ",yrhigh)
+print("yrlow        : ",yrlow)
+print("ttm_eps      : ",ttm_eps)
+print("ttm_pe       : ",ttm_pe)
+print("sector_pe    : ",sector_pe)
+print("bool_value   : ",bool_value)
+print("pb           : ",pb)
+print("face_value   : ",face_value)
+print("market_cap   : ",market_cap)
+print("divident     : ",divident)
+print("avg_voleme   : ",avg_voleme)
+print("avg_delivery : ",avg_delivery)
 
